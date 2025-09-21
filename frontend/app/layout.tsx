b699becb7a3c9750108b629f590f348deb32ec31
Laibah Shahid <laibahshahid18@gmail.com>
2025-09-21 17:53:46 +0500

    changed project directory and added home page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,8 +10,8 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "PrepWise",
-  description: "An AI-powered platform for preparing for mock interviews",
+  title: "PakTour AI",
+  description: "An AI-powered platform for smarter and safer Tourism in Pakistan",
 };
 
 export default function RootLayout({
